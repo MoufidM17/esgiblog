0b6dd7e847c0cff736b0072dc2eec90f0840cb68
MoufidM17 <mmoutarou1@myges.fr>
2024-01-10 05:53:04 +0100

    init project, layout, header, userMenu. Next prisma, model, session, sso.

---
+++ app/components/Header.tsx
@@ -0,0 +1,51 @@
+'use client'
+import { Box, Typography } from "@mui/joy"
+import Link from "next/link"
+import { usePathname, useRouter } from "next/navigation"
+import UserMenu from "./UserMenu"
+
+export default function Header () {
+    const router = useRouter()
+    const pathname = usePathname()
+
+    return (
+        <Box
+            component="header" className="Header"
+            sx={{
+                p: 2,
+                bgcolor: 'black',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                gridColumn: '1 / -1',
+                borderBottom: '1px solid',
+                borderColor: 'divider',
+                top: 0,
+                zIndex: 1100,
+                boxShadow: 'sm',
+            }}
+        >
+            <Link href="/">
+                <Typography
+                    onClick={() => pathname != "/" && router.push("/")}
+                    startDecorator={
+                        <img
+                            src="/appIcon.svg"
+                            loading="lazy"
+                            width="42"
+                            height="36"
+                            alt=""
+                        />
+                    }
+                    level="h4" 
+                    fontWeight="xl"
+                    sx={{color: 'white', cursor:'pointer'}}
+                >
+                    Esgiblog
+                </Typography>
+            </Link>
+            <UserMenu/>
+        </Box>
+    )
+}
