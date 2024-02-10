8c60bba8deeead65b20de15669c102226d3cc5b7
MoufidM17 <mmoutarou1@myges.fr>
2024-02-10 18:38:51 +0100

    PostPage : V4 (Edit button done). Next : Save button. DataFetching (server action)

--- app/posts/[pid]/page.tsx
+++ app/posts/[pid]/page.tsx
@@ -1,24 +1,31 @@
 "use client"
-import { IconButton, Stack, Typography } from "@mui/joy";
+import { IconButton, Modal, ModalClose, ModalDialog, Stack, Typography } from "@mui/joy";
 import { useEffect, useState } from "react";
+import { Edit2, Save } from "react-feather";
+import { useSession } from "next-auth/react";
 
 import { fetchPost } from "@/app/actions/post";
 import { PostType } from "@/app/common/types/posts";
 import { convertDateToString, toUppercaseFirstChar } from "@/app/lib/utils";
-import { useSession } from "next-auth/react";
-import { Edit2 } from "react-feather";
+import PostEditor from "@/app/components/common/postEditor";
 
 
 export default function PostItem({ params }: { params: { pid: string } }) {
     const { data: session } = useSession()
     const [post, setPost] = useState<PostType>(null)
+    const [description, setDescription] = useState<string>("")
+    const [openModal, setOpenModal] = useState<boolean>(false)
 
     const getPost = async (id: string) => {
         const data = await fetchPost({postId: id})
         setPost(data)
     }
     const handlePostEditButtonClick = () => {
+        setOpenModal(true)
+    }
 
+    const handlePostSaveButtonClick = () => {
+        
     }
     
     useEffect(() => {
@@ -27,7 +34,7 @@ export default function PostItem({ params }: { params: { pid: string } }) {
 
     
     return (
-        <Stack spacing={2}>
+        <Stack spacing={2} sx={{bgcolor: "#fff"}}>
             { post ? 
                 <Stack alignItems="center" sx={{pt: "1.5rem"}} spacing={3}>
                     <Stack>
@@ -39,11 +46,27 @@ export default function PostItem({ params }: { params: { pid: string } }) {
                 </Stack>
                 : <></>
             }
-            {session && session?.user?.email == post?.owner.email ? 
-                <IconButton onClick={handlePostEditButtonClick}>
-                    <Edit2></Edit2> Modifier
-                </IconButton>
+            {(session && session?.user?.email == post?.owner.email) ? 
+                <Stack direction="row" spacing={2} justifyContent="center">
+                    <IconButton sx={{    p: 1}} variant="outlined"  onClick={handlePostEditButtonClick}>
+                        <Edit2></Edit2> Modifier
+                    </IconButton>
+                    <IconButton sx={{bgcolor: "#000", p: 1}} variant="solid" onClick={handlePostSaveButtonClick}>
+                        <Save></Save> Enregistrer
+                    </IconButton>
+                </Stack>
             : <></>}
+            <Modal open={openModal} onClose={(_event: React.MouseEvent<HTMLButtonElement>, reason: string) => {
+                    setOpenModal(false);
+                }}
+            >
+                <ModalDialog layout={"fullscreen"} sx={{bgcolor: "#fff"}}>
+                    <ModalClose size="lg" sx={{bgcolor: "#000"}}/>
+                    <Stack  spacing={2} sx={{overflow: "auto", bgcolor: "#000", p: 2, mt:4}}>
+                        <PostEditor content={post?.description} onSave={setDescription}/>
+                    </Stack>
+                </ModalDialog>
+            </Modal>
         </Stack>
     )
 }
